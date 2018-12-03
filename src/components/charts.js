9ce4d049d365c2fad1c043db8e7bf2fdaa10758b
slavikzdebskyy <zdebskyy.slavik@gmail.com>
2018-12-03 20:35:30 +0200

    test

--- src/components/charts.js
+++ src/components/charts.js
@@ -3,7 +3,7 @@ import Highcharts from 'highcharts/highstock';
 import HighchartsReact from 'highcharts-react-official';
 import { connect } from 'react-redux';
 
-const Charts = (props) => {
+const Charts = props =>  {
   const options = {
     chart: {
       type: props.type
@@ -23,20 +23,24 @@ const Charts = (props) => {
       categories: ['Forks','Open issues', 'Score']
     },
     series: [...props.series]
-  };
+  }
   
   return (
   <HighchartsReact
-  highcharts={Highcharts}
-  constructorType={'chart'}
-  options={options}
-/>
-)}
+  highcharts = {Highcharts}
+  constructorType = {'chart'}
+  options = {options}
+  />
+  )
+}
+
 
 const mapStateToProps = store => {
-  return {series: store.series, 
+  return { 
+          series: store.series, 
           type: store.chartOptions.type, 
-          stacking: store.chartOptions.stacking}
+          stacking: store.chartOptions.stacking
+        }
 }
 
 export default connect(mapStateToProps)(Charts);
